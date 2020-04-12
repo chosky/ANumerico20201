a5f783f2a3cbb6d1f611377e44cd89b54b17577d
chosky <davidhenaoo98@gmail.com>
2020-04-11 19:17:34 -0500

    Se inicia el proyecto en netbeans, el proyecto actualmente está vacio

---
+++ ANumericoFinalProject/src/anumericofinalproject/ANumericoFinalProject.java
@@ -0,0 +1,19 @@
+package anumericofinalproject;
+
+/**
+ *
+ * @authors Jose David Henao Ocampo
+ *          Tomas Alvarez Gomez
+ *          Smith Alexis Carbajal Orozco
+ *          Alejandro Taborda Diaz
+ */
+public class ANumericoFinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
